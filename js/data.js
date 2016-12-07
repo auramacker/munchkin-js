a03b6296d0a560f84f9dce6d78b5cc71a21bd79a
auramacker <apple@MacBook-Air-Apple.local>
2016-12-07 14:35:26 +0300

    added monster constructor

--- js/data.js
+++ js/data.js
@@ -1,7 +1,28 @@
-function data(){
-  _CARD = {
-    name: "",
-    level: "",
-    sex: ""
+function data(){ // create all variables data for game
+  function Monster(obj){ // {id, img, name, description, numTreasures, monsterLevel, looseLevel, gender, race, class}
+    this.id = obj.id,
+    this.img = obj.img,
+    this.name = obj.name,
+    this.description = obj.description,
+    this.numTreasures = obj.numTreasures,
+    this.monsterLevel = obj.monsterLevel,
+    this.looseLevel = obj.looseLevel,
+    this.gender = obj.gender,
+    this.getGenderBonus = function(){
+      if (this.gender == "male") {
+        // male changes here
+      }
+      else {
+        // female changes here
+      }
+    },
+    this.race = obj.race,
+    this.getRaceBonus = function(){
+
+    },
+    this.class = obj.class,
+    this.getClaccBonus = function(){
+
+    } ,
   }
 }
