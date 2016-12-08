9807d38142d08f0a8b7284b22af085e2a7b26ac8
auramacker <apple@MacBook-Air-Apple.local>
2016-12-08 14:37:38 +0300

    Added all constructors

--- js/data.js
+++ js/data.js
@@ -1,27 +1,73 @@
 _MINLEVEL = 1;
 _MAXLEVEL = 10;
 _CARDSSIZE = 5;
-
-function Monster(obj, dice){ // {id, img, name, description, numTreasures, monsterLevel, looseLevel, gender, race, class}
-  this.id = obj.id,
-  this.img = obj.img,
-  this.name = obj.name,
-  this.description = obj.description,
-  this.numTreasures = obj.numTreasures,
-  this.monsterLevel = obj.monsterLevel,
-  this.looseLevel = obj.looseLevel,
-  this.gender = obj.gender,
-  this.getGenderBonus = function(){
-    if (this.gender == "male") {
-      this.monsterLevel += 1;
-    }
-  },
-  this.race = obj.race,
-  this.getRaceBonus = function(){
-
-  },
-  this.class = obj.class,
-  this.getClaccBonus = function(){
+// ********* Doors cards constructors *********
+function MonsterCard(objMonster, dice){ // {id, img, name, description, numTreasures, monsterLevel, looseLevel, gender, race, class}
+  this.id = objMonster.id,
+  this.img = objMonster.img,
+  this.name = objMonster.name,
+  this.description = objMonster.description,
+  this.numTreasures = objMonster.numTreasures,
+  this.monsterLevel = objMonster.monsterLevel,
+  this.looseLevel = objMonster.looseLevel,
+  this.gender = objMonster.gender,
+  this.genderBonus = objMonster.genderBonus,
+  this.race = objMonster.race,
+  this.raceBonus = objMonster.raceBonus,
+  this.class = objMonster.class,
+  this.classBonus = objMonster.classBonus
+}
+function CurseCard(objCurse){
+  this.id = objCurse.id,
+  this.name = objCurse.name,
+  this.image = objCurse.image,
+  this.description = objCurse.description,
+  this.cardEffect = objCurse.cardEffect
+}
+function ClassCard(objClassCard){
+  this.id = objClassCard.id,
+  this.name = objClassCard.name,
+  this.image = objClassCard.image,
+  this.description = objClassCard.description,
+  this.cardEffect = objClassCard.cardEffect
 
   }
+}
+function RaceCard(objRaceCard){
+  this.id = objRaceCard.id,
+  this.name = objRaceCard.name,
+  this.image = objRaceCard.image,
+  this.description = objRaceCard.description,
+  this.cardEffect = objRaceCard.cardEffect
+}
+
+// ********* End doors cards constructors *********
+// -------------------------------------------
+// ********* Treasures cards constructors *********
+
+function StuffCard(obj) {
+  this.id = obj.id,
+  this.name = obj.name,
+  this.image = obj.image,
+  this.description = obj.description,
+  this.slotType = obj.slotType,
+  this.power = obj.power,
+  this.cost = obj.cost,
+  this.effect = obj.effect,
+  this.isBig = obj.isBig
+}
+function PoisionCard(obj) {
+  this.id = obj.id,
+  this.name = obj.name,
+  this.image = obj.image,
+  this.description = obj.description,
+  this.effect = obj.effect
+}
+function BonusCard(objBonusCard) {
+  this.id = objBonusCard.id,
+  this.name = objBonusCard.name,
+  this.image = objBonusCard.image,
+  this.description = objBonusCard.description,
+  this.cardEffect = objBonusCard.cardEffect
+  this.isReusable = objBonusCard.cardIsReusable
 }
