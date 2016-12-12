ac2eed6d9e0cda66a6ce04994732051b4ed5efbe
auramacker <apple@MacBook-Air-Apple.local>
2016-12-12 16:47:39 +0300

    changed img prop

--- js/cards.js
+++ js/cards.js
@@ -35,8 +35,8 @@
 // ******** monster cards variables ********
   var monstersObjArr = [], createdMonstersArr = [];
   monstersObjArr[0] = {
-        id: 2,
-        img: _IMGPATH + "cards/card_01.png",
+        id: 021,
+        img: _IMGPATH + "/cards/" + this.id + ".png",
         name: "3872 орка",
         description: "+6 против дварфов (там старые счёты).",
         effect: "Брось кубик. На 2 и меньше ты затоптан до смерти, иначе потеряй столько уровней, сколько выпало.",
@@ -55,7 +55,7 @@
   var cursesObjArr = [], createdCursesArr = [];
   cursesObjArr[0] = {
         id: 21,
-        img: _IMGPATH + "cards/card_01.png",
+        img: _IMGPATH + this.id + ".png",
         name: "Утка обречённости",
         description: "Потеряй 2 уровня, чтобы неповадно было впредь подбирать птиц в подземельях.",
         cardEffect: "looseLevels2"
@@ -65,7 +65,7 @@
   var classesObjArr = [], createdClassesArr = [];
   classesObjArr[0] = {
         id: 11,
-        img: _IMGPATH + "cards/card_01.png",
+        img: _IMGPATH + this.id + ".png",
         name: "Воин",
         description: "Буйство: можешь сбросить до трёх карт в бою. Каждая даст тебе бонус +1. Ты побеждаешь при равенстве сил в бою.",
         cardEffect: "cardDropping+"
@@ -75,7 +75,7 @@
   var racesObjArr = [], createdRacesArr = [];
   racesObjArr[0] = {
         id: 20,
-        img: _IMGPATH + "cards/card_01.png",
+        img: _IMGPATH + this.id + ".png",
         name: "Дварф",
         description: "У тебя в игре может быть любое количество больших шмоток. В конце хода можешь оставлять на руке 6 карт.",
         cardEffect: "bigStuff" + _EFFECTSPLITTER + "manyCardsInHand"
@@ -87,7 +87,7 @@
   var stuffObjArr = [], createdStuffArr = [];
   stuffObjArr[0] = {
         id: 03,
-        img: _IMGPATH + "cards/card_01.png",
+        img: _IMGPATH + this.id + ".png",
         name: "Баклер бахвала",
         description: null,
         slotType: "oneHand",
@@ -101,7 +101,7 @@
   var poisionsObjArr = [], createdPoisionsArr = [];
   poisionsObjArr[0] = {
         id: 012,
-        img: _IMGPATH + "cards/card_01.png",
+        img: _IMGPATH + this.id + ".png",
         name: "Зелье холодильного взрыва",
         description: "Играй в любой бой. +3 любой стороне. Разовая шмотка.",
         effect: "anySidePlus+3"
@@ -111,7 +111,7 @@
   var bonusesObjArr = [], createdBonusesArr = [];
   bonusesObjArr[0] = {
         id: 02,
-        img: _IMGPATH + "cards/card_01.png",
+        img: _IMGPATH + this.id + ".png",
         name: "1000 голдов",
         description: "Получи уровень",
         cardEffect: "levelUp1",
