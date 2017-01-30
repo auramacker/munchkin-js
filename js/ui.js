78db23a7df29a0cc7141e22d7625956edf5e520e
auramacker <auramacker@gmail.com>
2017-01-30 12:11:33 +0300

    resize

--- js/ui.js
+++ js/ui.js
@@ -142,7 +142,7 @@ var ui = {
     $(window).on('load', function(){
       if (($(window).width() > 1360) && ($(window).height() > 600)) {
         $("body").css("width", $(window).width());
-        $("body").css("height", $(window).height());
+        $("body").css("height", $(window).height());//
       }
       else {
         $("body").css("width", "1360px");
